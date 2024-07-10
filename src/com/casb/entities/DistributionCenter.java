9835d2173b13ec29d24e29b35ee5e159ffc63fa5
casb62 <casb62@gmail.com>
2024-07-10 09:39:11 -0300

    DistributionCenter and Shelter classes modified.

--- src/com/casb/entities/DistributionCenter.java
+++ src/com/casb/entities/DistributionCenter.java
@@ -4,38 +4,25 @@ import java.util.Objects;
 
 public class DistributionCenter {
 
-	private Long id;
+	private Integer id;
 	private String name;
-	private String publicPlace;
-	private String number;
-	private String neighborhood;
-	private String city;
-	private String federationUnity;
-	private String zipCode;
-	private String sponsor;
+	private Integer addressId;
 	
 	public DistributionCenter() {
 	}
 
-	public DistributionCenter(Long id, String name, String publicPlace, String number, String neighborhood, String city,
-			String federationUnity, String zipCode, String sponsor) {
+	public DistributionCenter(Integer id, String name, Integer addressId) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.publicPlace = publicPlace;
-		this.number = number;
-		this.neighborhood = neighborhood;
-		this.city = city;
-		this.federationUnity = federationUnity;
-		this.zipCode = zipCode;
-		this.sponsor = sponsor;
+		this.addressId = addressId;
 	}
 
-	public Long getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -47,60 +34,12 @@ public class DistributionCenter {
 		this.name = name;
 	}
 
-	public String getPublicPlace() {
-		return publicPlace;
+	public Integer getAddressId() {
+		return addressId;
 	}
 
-	public void setPublicPlace(String publicPlace) {
-		this.publicPlace = publicPlace;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public String getNeighborhood() {
-		return neighborhood;
-	}
-
-	public void setNeighborhood(String neighborhood) {
-		this.neighborhood = neighborhood;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getFederationUnity() {
-		return federationUnity;
-	}
-
-	public void setFederationUnity(String federationUnity) {
-		this.federationUnity = federationUnity;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public String getSponsor() {
-		return sponsor;
-	}
-
-	public void setSponsor(String sponsor) {
-		this.sponsor = sponsor;
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
 	}
 
 	@Override
@@ -122,8 +61,6 @@ public class DistributionCenter {
 
 	@Override
 	public String toString() {
-		return "DistributionCenter [id=" + id + ", name=" + name + ", publicPlace=" + publicPlace + ", number=" + number
-				+ ", neighborhood=" + neighborhood + ", city=" + city + ", federationUnity=" + federationUnity
-				+ ", zipCode=" + zipCode + ", sponsor=" + sponsor + "]";
+		return "DistributionCenter [id=" + id + ", name=" + name + ", addressId=" + addressId + "]";
 	}
 }
--- src/com/casb/entities/Shelter.java
+++ src/com/casb/entities/Shelter.java
@@ -4,47 +4,31 @@ import java.util.Objects;
 
 public class Shelter {
 	
-	private Long id;
+	private Integer id;
 	private String name;
-	private String publicPlace;
-	private String number;
-	private String neighborhood;
-	private String city;
-	private String federationUnity;
-	private String zipCode;
 	private String sponsor;
 	private String phone;
 	private String email;
-	private Integer capacity;
-	private Integer amountOfPeople;
+	private Integer addressId;
 	
 	public Shelter() {
 	}
 
-	public Shelter(Long id, String name, String publicPlace, String number, String neighborhood, String city,
-			String federationUnity, String zipCode, String sponsor, String phone, String email, Integer capacity,
-			Integer amountOfPeople) {
+	public Shelter(Integer id, String name, String sponsor, String phone, String email, Integer addressId) {
 		super();
 		this.id = id;
 		this.name = name;
-		this.publicPlace = publicPlace;
-		this.number = number;
-		this.neighborhood = neighborhood;
-		this.city = city;
-		this.federationUnity = federationUnity;
-		this.zipCode = zipCode;
 		this.sponsor = sponsor;
 		this.phone = phone;
 		this.email = email;
-		this.capacity = capacity;
-		this.amountOfPeople = amountOfPeople;
+		this.addressId = addressId;
 	}
 
-	public Long getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(Long id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -54,54 +38,6 @@ public class Shelter {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String getPublicPlace() {
-		return publicPlace;
-	}
-
-	public void setPublicPlace(String publicPlace) {
-		this.publicPlace = publicPlace;
-	}
-
-	public String getNumber() {
-		return number;
-	}
-
-	public void setNumber(String number) {
-		this.number = number;
-	}
-
-	public String getNeighborhood() {
-		return neighborhood;
-	}
-
-	public void setNeighborhood(String neighborhood) {
-		this.neighborhood = neighborhood;
-	}
-
-	public String getCity() {
-		return city;
-	}
-
-	public void setCity(String city) {
-		this.city = city;
-	}
-
-	public String getFederationUnity() {
-		return federationUnity;
-	}
-
-	public void setFederationUnity(String federationUnity) {
-		this.federationUnity = federationUnity;
-	}
-
-	public String getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(String zipCode) {
-		this.zipCode = zipCode;
 	}
 
 	public String getSponsor() {
@@ -128,22 +64,14 @@ public class Shelter {
 		this.email = email;
 	}
 
-	public Integer getCapacity() {
-		return capacity;
+	public Integer getAddressId() {
+		return addressId;
 	}
 
-	public void setCapacity(Integer capacity) {
-		this.capacity = capacity;
+	public void setAddressId(Integer addressId) {
+		this.addressId = addressId;
 	}
 
-	public Integer getAmountOfPeople() {
-		return amountOfPeople;
-	}
-
-	public void setAmountOfPeople(Integer amountOfPeople) {
-		this.amountOfPeople = amountOfPeople;
-	}
-	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -160,12 +88,10 @@ public class Shelter {
 		Shelter other = (Shelter) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 	@Override
 	public String toString() {
-		return "Shelter [id=" + id + ", name=" + name + ", publicPlace=" + publicPlace + ", number=" + number
-				+ ", neighborhood=" + neighborhood + ", city=" + city + ", federationUnity=" + federationUnity
-				+ ", zipCode=" + zipCode + ", sponsor=" + sponsor + ", phone=" + phone + ", email=" + email
-				+ ", capacity=" + capacity + ", amountOfPeople=" + amountOfPeople + "]";
+		return "Shelter [id=" + id + ", name=" + name + ", sponsor=" + sponsor + ", phone=" + phone + ", email=" + email
+				+ ", addressId=" + addressId + "]";
 	}
 }
